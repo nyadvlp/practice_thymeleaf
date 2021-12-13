a85f92a4ff7f71e3bb4e3f97792d1bfb9a6c3c8a
nya.dvlp@gmail.com <nya.dvlp@gmail.com>
2021-12-13 15:02:36 +0900

    fruit2

--- src/main/java/com/example/demo/fruit/controller/FruitController.java
+++ src/main/java/com/example/demo/fruit/controller/FruitController.java
@@ -12,13 +12,24 @@ public class FruitController {
 	
 	@GetMapping("/fruit")
 	public String getFruit(Model model) {
-		
+		System.out.println("/fruit");
 		Map<String, String> fruitMap = new HashMap<String, String>();
 		fruitMap.put("fruit1", "apple");
 		fruitMap.put("fruit2", "banana");
 		fruitMap.put("fruit3", "pineapple");
 		model.addAttribute("fruit", fruitMap);
 		return "fruit/fruit";
+	}
+	
+	@GetMapping("/fruit2")
+	public String getFruit2(Model model) {
+		System.out.println("/fruit2");
+		Map<String, String> fruitMap = new HashMap<String, String>();
+		fruitMap.put("fruit1", "apple");
+		fruitMap.put("fruit2", "banana");
+		fruitMap.put("fruit3", "pineapple");
+		model.addAttribute("fruit", fruitMap);
+		return "fruit/fruit2";
 	}
 
 }
